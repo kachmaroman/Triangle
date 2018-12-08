09dba4f89542a4f6f429f9909159fb7232bc6c09
Roman Kachmar <lnt397@gmail.com>
2018-12-08 22:36:13 +0200

    init

---
+++ ComputerGraphics/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ComputerGraphics
+{
+	sealed partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBoxGrid = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrid)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBoxGrid
+			// 
+			this.pictureBoxGrid.Location = new System.Drawing.Point(12, 12);
+			this.pictureBoxGrid.Name = "pictureBoxGrid";
+			this.pictureBoxGrid.Size = new System.Drawing.Size(800, 800);
+			this.pictureBoxGrid.TabIndex = 1;
+			this.pictureBoxGrid.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(826, 827);
+			this.Controls.Add(this.pictureBoxGrid);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrid)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.PictureBox pictureBoxGrid;
+	}
+}
+
---
+++ ComputerGraphics/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerGraphics
+{
+	public sealed partial class Form1 : Form
+	{
+		private const int NumOfCells = 100;
+		private const int CellSize = 20;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			Bitmap image = new Bitmap(pictureBoxGrid.Width, pictureBoxGrid.Height, PixelFormat.Format32bppRgb);
+			Pen pen = new Pen(Color.Gray, 1);
+
+			DrawGrid(image, pen);
+
+			pen.Color = Color.Black;
+			pen.Width = 3;
+
+			DrawCoordinateSystem(image, pen);
+			DrawLabels(image);
+			
+			pictureBoxGrid.Invalidate();
+		}
+
+		private void DrawGrid(Bitmap image, Pen pen)
+		{
+			Graphics.FromImage(image).Clear(Color.White);
+			pictureBoxGrid.BackgroundImage = image;
+
+			for (int i = 0; i < pictureBoxGrid.Height; i++)
+			{
+				// Vertical
+				Graphics.FromImage(image).DrawLine(pen, i * CellSize, 0, i * CellSize, NumOfCells * CellSize);
+
+				// Horizontal
+				Graphics.FromImage(image).DrawLine(pen, 0, i * CellSize, NumOfCells * CellSize, i * CellSize);
+			}
+		}
+
+		private void DrawCoordinateSystem(Bitmap image, Pen pen)
+		{
+			// Vertical
+			Graphics.FromImage(image).DrawLine(pen, pictureBoxGrid.Width / 2, 0, pictureBoxGrid.Width / 2, pictureBoxGrid.Height);
+
+			// Horizontal
+			Graphics.FromImage(image).DrawLine(pen, 0, pictureBoxGrid.Height / 2, pictureBoxGrid.Width, pictureBoxGrid.Height / 2);
+
+			// Arrows
+			Graphics.FromImage(image).DrawLine(pen, pictureBoxGrid.Width / 2, 0, pictureBoxGrid.Width / 2 - 10, 10);
+			Graphics.FromImage(image).DrawLine(pen, pictureBoxGrid.Width / 2, 0, pictureBoxGrid.Width / 2 + 10, 10);
+
+			Graphics.FromImage(image).DrawLine(pen, pictureBoxGrid.Width, pictureBoxGrid.Height / 2, pictureBoxGrid.Width - 10, pictureBoxGrid.Height / 2 - 10);
+			Graphics.FromImage(image).DrawLine(pen, pictureBoxGrid.Width, pictureBoxGrid.Height / 2, pictureBoxGrid.Width - 10, pictureBoxGrid.Height / 2 + 10);
+		}
+
+		private void DrawLabels(Bitmap image)
+		{
+			int maxNum = 10;
+			int number = pictureBoxGrid.Height / 2 - 45;
+
+			for (int i = 1; i < maxNum; i++)
+			{
+				Graphics.FromImage(image).DrawString($"{i}", new Font(Font, FontStyle.Italic), Brushes.Black, pictureBoxGrid.Width / 2 - 25, number);
+				Graphics.FromImage(image).DrawString($"{-i}", new Font(Font, FontStyle.Italic), Brushes.Black, number, pictureBoxGrid.Height / 2 + 10);
+				number -= 40;
+			}
+
+			number = pictureBoxGrid.Width / 2 + 30;
+
+			for (int i = 1; i < maxNum; i++)
+			{
+				Graphics.FromImage(image).DrawString($"{i}", new Font(Font, FontStyle.Italic), Brushes.Black, number, pictureBoxGrid.Height / 2 + 10);
+				Graphics.FromImage(image).DrawString($"{-i}", new Font(Font, FontStyle.Italic), Brushes.Black, pictureBoxGrid.Width / 2 + 10, number);
+				number += 40;
+			}
+
+			//Graphics.FromImage(image).DrawRectangle(pen, new Rectangle(760, 240, 50, 50));
+		}
+	}
+}
